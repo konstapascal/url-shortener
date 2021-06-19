14f9c25f56bfd9a14397a6e4f54354ee5a03ccab
konstapascal <pascalkonstantinos@gmail.com>
2021-06-20 01:09:26 +0200

    fixed generate section in NotFound

--- src/components/ListUrls.js
+++ src/components/ListUrls.js
@@ -4,7 +4,7 @@ import GeneratedLinkRow from './GeneratedLinkRow';
 function ListUrls() {
 	return (
 		<div className='bg-light-200 dark:bg-dark-200 text-font-black rounded-b-md dark:text-font-white sm:px-8 lg:px-16 w-full px-4 pb-20 text-center transition-colors duration-500'>
-			<h3 className='lg:text-4xl mb-16 text-3xl font-semibold'>Generated Links</h3>
+			<h3 className='lg:text-4xl mb-16 text-3xl font-semibold'>Generated Links List</h3>
 
 			{/* <NoLinksWarning /> */}
 
