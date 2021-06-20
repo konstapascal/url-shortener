52b053a72afd2af122457064739be9cafb190e45
konstapascal <pascalkonstantinos@gmail.com>
2021-06-21 00:23:15 +0200

    changed text

--- src/components/RedirectLoading.js
+++ src/components/RedirectLoading.js
@@ -2,7 +2,7 @@ export const RedirectLoading = () => {
 	return (
 		<div className='mt-60 lg:mt-72 lg:flex-row flex flex-col items-center justify-center'>
 			<div id='lds-dual-ring' />
-			<p className='lg:text-4xl lg:mt-0 lg:ml-6 mt-8 text-3xl'>Te iubesc mult!</p>
+			<p className='lg:text-4xl lg:mt-0 lg:ml-6 mt-8 text-3xl'>Redirecting...</p>
 		</div>
 	);
 };
