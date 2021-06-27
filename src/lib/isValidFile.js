91c33b2497455905ac66726323e6f6e42b8eb0b2
konstapascal <pascalkonstantinos@gmail.com>
2021-06-27 03:20:26 +0200

    added edge cases for null in file check

--- src/lib/isValidFile.js
+++ src/lib/isValidFile.js
@@ -3,7 +3,7 @@ export default function isValidFile(fileContents) {
 	if (!isArray) return false;
 
 	const isArrayOfValidObjs = fileContents.every(entry => {
-		const isEntryObj = Object.getPrototypeOf(entry).constructor === Object;
+		const isEntryObj = !!entry && entry.constructor === Object;
 		const isEntryObjValid = entry.url && entry.slug;
 
 		return isEntryObj && isEntryObjValid;
