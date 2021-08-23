9caa3c6eecff5f157defd93b2a0a2692d219fe88
konstapascal <pascalkonstantinos@gmail.com>
2021-08-23 19:04:09 +0200

    added additional check to importing

--- src/lib/isValidFile.js
+++ src/lib/isValidFile.js
@@ -4,13 +4,14 @@ export default function isValidFile(fileContent) {
 	const isArray = Array.isArray(fileContent);
 	if (!isArray) return false;
 
-	const isArrayOfValidObjs = fileContent.every(obj => {
-		const isObjValid = !!obj && obj.constructor === Object;
-		const areObjKeysValid = 'url' in obj && 'slug' in obj;
-		const isUrlValid = isUrl(obj.url);
-		const isSlugValid = obj.slug !== '' && !obj.slug.includes(' ');
+	const isArrayOfValidObjs = fileContent.every(elem => {
+		const isObjValid = !!elem && elem.constructor === Object;
+		const areObjKeysValid = 'url' in elem && 'slug' in elem;
+		const isUrlValid = isUrl(elem.url);
+		const isSlugString = typeof elem.slug === 'string';
+		const isSlugValid = elem.slug !== '' && !elem.slug.includes(' ');
 
-		return isObjValid && areObjKeysValid && isUrlValid && isSlugValid;
+		return isObjValid && areObjKeysValid && isUrlValid && isSlugString && isSlugValid;
 	});
 
 	return isArrayOfValidObjs;
